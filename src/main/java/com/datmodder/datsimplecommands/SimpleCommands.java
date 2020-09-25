00d031a6ea2ba90532df9f973ad867dfaec4a135
Jacob <jtljac2@gmail.com>
2020-09-25 01:52:56 +0100

    Changed modid

--- src/main/java/com/datmodder/datsimplecommands/SimpleCommands.java
+++ src/main/java/com/datmodder/datsimplecommands/SimpleCommands.java
@@ -19,9 +19,9 @@ import org.apache.logging.log4j.Logger;
 )
 public class SimpleCommands {
 
-    public static final String MOD_ID = "simplecommands";
+    public static final String MOD_ID = "datsimplecommands";
     public static final String MOD_NAME = "Simple Commands";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
