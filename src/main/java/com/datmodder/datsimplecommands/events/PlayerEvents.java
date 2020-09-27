d55e74cc7d6810fe4158b458bfb494ae2dc7c622
Jacob <jtljac2@gmail.com>
2020-09-27 16:12:07 +0100

    Updated dat modder's api, added death saving for /back

--- src/main/java/com/datmodder/datsimplecommands/events/PlayerEvents.java
+++ src/main/java/com/datmodder/datsimplecommands/events/PlayerEvents.java
@@ -2,7 +2,11 @@ package com.datmodder.datsimplecommands.events;
 
 import com.datmodder.datsimplecommands.SimpleCommands;
 import com.datmodder.datsimplecommands.utils.PlayerManager;
+import com.datmodder.datsimplecommands.utils.structures.PlayerData;
+import com.demmodders.datmoddingapi.structures.Location;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
@@ -23,5 +27,13 @@ public class PlayerEvents {
         UUID playerID = e.player.getUniqueID();
         PlayerManager pMan = PlayerManager.getInstance();
         pMan.unloadPlayer(playerID);
+    }
+
+    @SubscribeEvent
+    public static void playerDeath(LivingDeathEvent e) {
+        if (e.getEntity() instanceof EntityPlayerMP) {
+            EntityPlayerMP player = (EntityPlayerMP) e.getEntity();
+            PlayerManager.getInstance().updatePlayerBackLocation(player.getUniqueID(), new Location(player.dimension, player.posX, player.posY, player.posZ, player.rotationPitch, player.rotationYaw));
+        }
     }
 }
